ae9de6bcaee7a73c1bef3ab9c76e1dd45bc3aba6
lindberg.junior <lindberg_junior@hotmail.com>
2017-11-14 01:06:36 -0200

    trabalho2

---
+++ Exercicios/trabalho2-lindberg/index.js
@@ -0,0 +1,76 @@
+var groups = [];
+var messages = [];
+
+
+function getGroups(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            groups = JSON.parse(this.responseText);
+            makeUL(groups);
+        }
+    };
+    xhttp.send();    xhttp.open('GET','http://rest.learncode.academy/api/camomillar/groups',true);
+
+}
+
+getGroups();
+
+function clearMessages() {
+    var list_m = document.getElementById('mensagem');
+    list_m.innerHTML = "";
+}
+
+function getMessages(groupid) {
+    messages = [];
+    clearMessages();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            messages = JSON.parse(this.responseText);
+            makeMsgs(messages);
+        }
+    };
+    xhttp.open('GET', 'http://rest.learncode.academy/api/camomillar/'+groupid, true);
+    xhttp.send();
+}
+
+
+function makeUL(array) {
+    var list = document.getElementById('amigosList');
+
+    for(var i = 0; i < array.length; i++) {
+        var item = document.createElement('li');
+        var link = document.createElement('a'); //esse é o elemento link
+        var img = document.createElement('img');
+        img.src = 'person.png';
+        img.width = 24;
+        img.height = 24;
+        link.appendChild(img);
+        link.className += " pointer";
+        link.appendChild(document.createTextNode(' ' + array[i].groupName));
+        link.onclick = (function () {
+            var currentI = i;
+            return function(){
+                getMessages(array[currentI].groupID);
+                document.getElementById('nome-amigo').innerHTML = array[currentI].groupName;
+            };
+        })();
+        item.appendChild(link);
+        list.appendChild(item);
+    }
+    return list;
+}
+
+
+function makeMsgs(msg){
+    var list_m = document.getElementById('mensagem');
+    for (var j = 0; j < msg.length; j++) {
+        var h3 = document.createElement('h3');
+        h3.appendChild(document.createTextNode(msg[j].userName));
+        var p = document.createElement('p');
+        p.appendChild(document.createTextNode(msg[j].message));
+        list_m.appendChild(h3);
+        list_m.appendChild(p);
+    }
+}
